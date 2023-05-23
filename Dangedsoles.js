af6dfb16c1d623020e11aa335739b83cf16d952f
Mason <113492401+HumorFactory@users.noreply.github.com>
2023-05-22 22:58:33 -0700

    asdfghjkl;'

--- Dangedsoles.js
+++ Dangedsoles.js
@@ -5,9 +5,9 @@ var current_weapon = "stick";
 var enemy_integer = 1;
 var weapon_integer = 1;
 
-var enemy_theme = new Audio("media/Audio/enemy-theme.ogg");
+var enemy_theme = new Audio("Media/Audio/enemy-theme.ogg");
 
-var enemyHurtArray = [new Audio("media/Audio/Ow1.mp3"), new Audio("media/Audio/Ow2.mp3"), new Audio("media/Audio/Ow3.mp3"), new Audio("media/Audio/Ow4.mp3")];
+var enemyHurtArray = [new Audio("Media/Audio/Ow1.mp3"), new Audio("Media/Audio/Ow2.mp3"), new Audio("Media/Audio/Ow3.mp3"), new Audio("Media/Audio/Ow4.mp3")];
 // enemy list
 var EnemyList = {
     1: {
@@ -19,9 +19,9 @@ var EnemyList = {
         "soles": 10, //Soles
     },
     2: {
-        "name": "Fishy",
+        "name": "Big Black Corroder",
         "image_dancing": "Media/images/DancingBBC.gif",
-        "image_hurt": "Media/images/HurtBBC.png",
+        "image_hurt": "Media/images/HurtFish.png",
         "health": 200,
         "alive": true,
         "soles": 20,
@@ -111,8 +111,8 @@ function theme_loop(status) {
 
 function summon_enemy(name) {
     current_enemy = name;
-    enemy_wrapper.innerHTML += `<div id="current-enemy"><button class="damage-button" id="damage-enemy" onclick="damageEnemy()"><img id="enemy-image" src="${EnemyList[enemy_integer].image_dancing}"></button><p id="enemy-name">${EnemyList[enemy_integer].name}</p></span></div>`;
-    document.getElementById("enemy-container").insertAdjacentHTML("beforebegin", `<progress id="enemy-health" value="${EnemyList[enemy_integer].health}" max="${EnemyList[enemy_integer].health}"></progress>`) 
+    enemy_wrapper.innerHTML += `<div id="current-enemy"><button class="damage-button" id="damage-enemy" onclick="damageEnemy()"><img id="enemy-image" src="${EnemyList[enemy_integer].image_dancing}"></button></span><img class="weapon" id="weapon-image" src="${WeaponList[weapon_integer].image_default}"></div>`;
+    document.getElementById("enemy-container").insertAdjacentHTML("beforebegin", `<progress id="enemy-health" value="${EnemyList[enemy_integer].health}" max="${EnemyList[enemy_integer].health}"></progress><p id="enemy-name">${EnemyList[enemy_integer].name}</p>`) 
 }
 
 function summon_weapon(name) {
@@ -139,7 +139,8 @@ function damageEnemy() {
         if(document.getElementById("enemy-health").value <= 0) {
             enemy_wrapper.innerHTML = "";
             document.getElementById("enemy-health").remove();
-            var death = new Audio("media/audio/Souls.mp3");
+            document.getElementById("enemy-name").remove();
+            var death = new Audio("Media/audio/Souls.mp3");
             death.play();
             enemy_integer++;
             summon_enemy(EnemyList[enemy_integer]);
